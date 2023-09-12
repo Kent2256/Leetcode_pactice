3b61b9ad9bf0d94d607481158d0e374e3ae16d17
Kent2256 <changkuyu@gmail.com>
2023-09-12 23:50:49 +0800

    update

--- 141_linkedlist_cycle.py
+++ 141_linkedlist_cycle.py
@@ -8,6 +8,6 @@ class Solution(object):
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
-            if slow == fast:
+        if slow == fast:
                 return True
         return False
